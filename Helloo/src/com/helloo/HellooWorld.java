afa9e2a97e8f423f2ed8b1c6f058e314d15292ea
Jyoti <jsrout9098@gmail.com>
2020-10-15 22:34:36 +0530

    hi..I hv chnged soome code

--- Helloo/src/com/helloo/HellooWorld.java
+++ Helloo/src/com/helloo/HellooWorld.java
@@ -4,6 +4,9 @@ public class HellooWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Jayaguru");
+		System.out.println("abcdef");
+		System.out.println("mnopq");
+		System.out.println("jay jay");
 	}
 
 }
